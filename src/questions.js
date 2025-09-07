c786920ee06b3e896693a4cd783beb0bf82455c2
karnamfarha34 <karnamfarha34@gmail.com>
2025-09-07 20:57:09 +0530

    Updated React project files

--- src/questions.js
+++ src/questions.js
@@ -5,47 +5,114 @@
 const questions=[            
   {                                 
     id:1,
-    question:'Why are you still alive?',         
-    answer:'pagal hun main',
+    question:'Which branch are you the Head of Department?',         
+    answer:'Artificial Intelligence',
     options:[
-      'i love me',
-      'mumma said so',
-      'clg attendence',
-      'pagal hun main'
+      'Computer Science',
+      'Artificial Intelligence',
+      'Civil Engineering',
+      'Mechanical Engineering'
     ]
   },
   {
     id:2,
-    question:'What is your name?',
-    answer:'chuchundar',
+    question:'Which programming language is most recommended for beginners in AI?',
+    answer:'Python',
     options:[
-      'chuchundar',
-      'bubble gum',
-      'banana',
-      'ass'
+      'Java',
+      'C',
+      'Python',
+      'C++'
     ]
   },
   {
     id:3,
-    question:'why are you like this?',
-    answer:'mentally unnstable',
+    question:'Which of the following frameworks is widely used for Deep Learning?',
+    answer:'PyTorch',
     options:[
-      'mentally unnstable',
-      'oniii chann',
-      'because im crazy af',
-      'its my choice'
+      'PyTorch',
+      'MySQL',
+      'Hadoop',
+      'Tableau'
     ]
   },
   {
     id:4,
-    question:'bubble gum?',
-    answer:'nope, im chingam',
+    question:'Which area of AI deals with self-driving cars?',
+    answer:'Computer Vision',
     options:[
-      'haan bol',
-      'nope, im chingam',
-      'chabaa dalungi',
-      'none of these'
+      'Data Mining',
+      'Expert Systems',
+      'Virtual Reality',
+      'Computer Vision'
+    ]
+  },
+  {
+  id:5,
+    question:'Which language is used to structure web pages?',
+    answer:'HTML',
+    options:[
+      'CSS',
+      'HTML',
+      'JavaScript',
+      'Python'
+    ]
+  },
+  {
+  id:6,
+    question:'Which language is used to style web pages?',
+    answer:'CSS',
+    options:[
+      'HTML',
+      'Java',
+      'CSS',
+      'C++'
+    ]
+  },
+  {
+  id:7,
+    question:'What does CSS stand for?',
+    answer:'Cascading Style Sheets',
+    options:[
+      'Create Style System',
+      'Computer Style Sheets',
+      'Colorful Style Script',
+      'Cascading Style Sheets'
+    ]
+  },
+  {
+  id:8,
+    question:'Which of the following is a popular JavaScript library?',
+    answer:'React',
+    options:[
+      'React',
+      'Flask',
+      'Laravel',
+      'Django'
+    ]
+  },
+  {
+  id:9,
+    question:'Which tag is used to display an image in HTML?',
+    answer:'<img>',
+    options:[
+      '<image>',
+      '<img>',
+      '<picture>',
+      '<src>'
+    ]
+  },
+  {
+  id:10,
+    question:'Which of these is used for the backend of web applications?',
+    answer:'Node.js',
+    options:[
+      'CSS',
+      'HTML',
+      'Node.js',
+      'Bootstrap'
     ]
   }
+  
 ]
 export default questions;
